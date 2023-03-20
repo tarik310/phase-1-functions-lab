3552494e11d6ff5401cc3340aa32e5e7fe1aeb90
Tareq Harh <tareq310harh@gmail.com>
2023-03-20 16:10:22 +0300

    all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+  }
+  
+  function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+      return 0;
+    } else if (distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
